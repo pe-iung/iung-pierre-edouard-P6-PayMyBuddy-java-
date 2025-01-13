4069f3be4a05da5512fc8370b333c23b886ddc83
pierre-edouard iung <contact@pierre-edouard.tech>
2025-01-13 14:05:09 +0100

    chore: rename repository for clarity

---
+++ src/main/java/com/P6/P6/repositories/UserEntityRepository.java
@@ -0,0 +1,14 @@
+package com.P6.P6.repositories;
+
+import com.P6.P6.model.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserEntityRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByUsername(String username);
+
+}
+
