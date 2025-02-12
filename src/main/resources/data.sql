0b25ea8fecf7e12f96b094bddc4fe063fa579365
pierre-edouard iung <contact@pierre-edouard.tech>
2025-02-12 20:08:32 +0100

    chore: add testing user in database when loading the app

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-/*INSERT INTO user_table (username, password, email) VALUES ('dbuser', '$2a$10$M6fHeE2TP4sh3pdSyiNf6Oj79YkqF9vg0706oeNCq5gmfYcZgKnDq', 'USER@email.com'),
+INSERT INTO user_table (username, password, email) VALUES ('dbuser', '$2a$10$M6fHeE2TP4sh3pdSyiNf6Oj79YkqF9vg0706oeNCq5gmfYcZgKnDq', 'USER@email.com'),
                                                      ('dbadmin', '$2a$10$1vG8phlPmXHUQtm3Xw3efOzegqx7RsdrlswUIWkKPIofZIx3X3gvW', 'ADMIN@email.com');
 
 INSERT INTO public.user_friends (friend_id, user_entity_id) VALUES (2, 1);
@@ -6,4 +6,3 @@ INSERT INTO public.user_friends (friend_id, user_entity_id) VALUES (2, 1);
 INSERT INTO public.account_table (balance, user_id, id) VALUES (1000, 1, 1);
 
 INSERT INTO public.account_table (balance, user_id, id) VALUES (500, 2, 2);
-*/
